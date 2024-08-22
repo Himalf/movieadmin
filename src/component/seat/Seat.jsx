7014d3d6bf0b8f8542f06516b589a6dc9ca8fb84
himalf <himal.fullel15@gmail.com>
2024-08-22 17:57:33 +0545

    dynamic seat allocation

--- src/component/seat/Seat.jsx
+++ src/component/seat/Seat.jsx
@@ -11,18 +11,18 @@ const Seat = () => {
   return (
     <main className="mx-auto p-4">
       <section className="flex space-x-4 justify-center mb-4">
-        <button
+        {/* <button
           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors"
           onClick={handleAddClick}
         >
           Add Seat
-        </button>
-        <button
+        </button> */}
+        {/* <button
           className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-colors"
           onClick={handleViewClick}
         >
           View Seat
-        </button>
+        </button> */}
       </section>
 
       <div className="text-center">
